531b70d71e6e7121f41a060e0c9f522cac0b10a3
IRADUKUNDA Pacific Rugwizangoga <pacific.r.iradukunda@gmail.com>
2023-10-22 22:45:25 +0200

    adds a Navbar

---
+++ hotels-and-co/app/components/Avatar.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+import Image from 'next/image'
+const Avatar = () => {
+  return (
+    <Image className='rounded-full'
+    height={30}
+    width={30}
+    alt='Avatar'
+    src='/images/user.jpg'
+    />
+  )
+}
+
+export default Avatar
