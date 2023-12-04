91c36c98eedf181dd74fef1a6fb7593fb3c863f1
IRADUKUNDA Pacific Rugwizangoga <pacific.r.iradukunda@gmail.com>
2023-12-04 18:36:55 +0200

    renders card with an infinite scroll

--- app/actions/Listings.ts
+++ app/actions/Listings.ts
@@ -1,16 +1,15 @@
 'use server'
 import axios from "axios";
 
-export const getListings = (pagePram: number) => {
-  const ListingsURL = new URL ('https://65338eb8d80bd20280f69405.mockapi.io/api/Rooms');
-  ListingsURL.searchParams.append('completed', 'false');
-  ListingsURL.searchParams.append('page', pagePram.toString());
-  ListingsURL.searchParams.append('limit', '3');
+export const listings = async ({pageParam}:{pageParam:number}) => {
+  const limit = 8;
+  const ListingsURL = `https://65338eb8d80bd20280f69405.mockapi.io/api/Rooms?page=${pageParam}&limit=${limit}`;
 
-  return axios.get(ListingsURL.toString()).then((res)=>res.data);
-};
-
-export const getListing = (id:any) => {
-  const ListingURL = 'https://65338eb8d80bd20280f69405.mockapi.io/api/Rooms/'+`${id}`;
-  return axios.get(ListingURL).then((res)=>res.data);
+  try {
+    const response = await axios.get(ListingsURL);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching listings:', error);
+    throw error;
+  }
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,80 @@
-
+'use client'
 import Container from "@/app/components/Container";
-import { getListings } from "./actions/Listings";
-import { LoadMore } from "./components/hooks/loadMore";
-import { Listings } from "./components/listings/Listings";
+import { listings } from "./actions/Listings";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
+import useIntersectionObserver from "./components/hooks/useIntersectionObserver";
+import { Spinner } from "./components/Spinner";
+import { useInfiniteQuery } from '@tanstack/react-query'
+import React from "react";
 
-const Home = async() => {
+const Home = () => {
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    status,
+  } = useInfiniteQuery({
+    queryKey: ['rooms'],
+    queryFn: listings,
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages, lastPageParam) => {
+    if (lastPage.length === 0) {
+        return undefined
+    }
+    return lastPageParam + 1
+  },
+  })
+  const lastListingRef = useIntersectionObserver<HTMLDivElement
+  >(() => {
+    if (hasNextPage) {
+      void fetchNextPage();
+    }
+  }, [hasNextPage]);
 
-  const listings = await getListings(1) 
+  if (status === 'pending') {
+    return <p>Loading...</p>;
+  }
 
+  if (status === 'error') {
+    return <p>Error: {error.message}</p>;
+  }
+
+  if (!data || data?.pages[0].length === 0) {
+    return <EmptyState showReset /> // or another loading state if needed
+  }
   return (
-      <Container>
-        
-        <Listings listings={listings} />
-        <LoadMore />
-      </Container>
+    <Container>
+      <div
+         className="
+           pt-24
+           grid 
+           grid-cols-1 
+           sm:grid-cols-2 
+           md:grid-cols-3 
+           lg:grid-cols-4
+           xl:grid-cols-4 
+           2xl:grid-cols-5
+           gap-8
+         "
+       >
+      {data.pages.map((group, pageIndex) => (
+          <React.Fragment key={pageIndex}>
+            {group.map((listing: any, i: any) => (
+              <ListingCard
+                key={listing.id}
+                data={listing}
+                ref={(data.pages.length - 1 === pageIndex && group.length - 1 === i) ? lastListingRef : null}
+              />
+            ))}
+          </React.Fragment>
+        ))}
+    </div>
+    {hasNextPage && isFetching ? <Spinner /> : null}
+    </Container>
   )
-}
+};
 
 export default Home;
